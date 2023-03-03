4eaf00cd1600bcbf84ad264e91f5e02c979dadf2
Jason Martinez <66093119+martinezjas@users.noreply.github.com>
2023-03-03 18:48:13 -0500

    added fullscreen button and ending indicator

--- static/js/himnario_play.js
+++ static/js/himnario_play.js
@@ -20,13 +20,18 @@ audio.addEventListener(
     var curInterval = setInterval(function () {
       if (i < res.length) {
         if (currentTime == res[i].timestamp - 1) {
-          document.getElementById("lyrics").innerHTML = res[i].verse.content.replace(/(\.\s+|;\s+)/g,"\$1<br>")
+          document.getElementById("lyrics").innerHTML = res[
+            i
+          ].verse.content.replace(/(\.\s+|;\s+|!\s+|\?\s+)/g, "$1<br>");
           verse_number = res[i].verse.number;
           if (verse_number == 0) {
             verse_number = "Coro";
           }
           document.getElementById("verseno").innerHTML = verse_number;
           i = i + 1;
+          if (i == res.length) {
+            document.getElementById("end_icon").hidden = false;
+          }
         }
       } else {
         clearInterval(curInterval);
@@ -35,3 +40,26 @@ audio.addEventListener(
   },
   false
 );
+let myDocument = document.documentElement;
+let myButton = document.getElementById("fullscreen-btn");
+
+myButton.addEventListener("click", function () {
+  if ((document.fullScreenElement && document.fullScreenElement !== null) ||    
+   (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+    if (document.documentElement.requestFullScreen) {  
+      document.documentElement.requestFullScreen();  
+    } else if (document.documentElement.mozRequestFullScreen) {  
+      document.documentElement.mozRequestFullScreen();  
+    } else if (document.documentElement.webkitRequestFullScreen) {  
+      document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);  
+    }  
+  } else {  
+    if (document.cancelFullScreen) {  
+      document.cancelFullScreen();  
+    } else if (document.mozCancelFullScreen) {  
+      document.mozCancelFullScreen();  
+    } else if (document.webkitCancelFullScreen) {  
+      document.webkitCancelFullScreen();  
+    }  
+  }  
+});
